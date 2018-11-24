25cc389fd99060be17730600ea02e7bd74f6818f
daf111 <daf111@yahoo.com.ar>
2018-11-24 09:43:44 -0300

    Leaflet-MousePosition DMS
    Se muestra las coordenadas del puntero en grados, minutos y segundos.

--- js/mapa.js
+++ js/mapa.js
@@ -41,7 +41,19 @@ function onEachFeature(feature, layer) {
 }
 
 // Leaflet-MousePosition plugin https://github.com/ardhi/Leaflet.MousePosition
-L.control.mousePosition({ position: 'bottomright', }).addTo(mapa);
+L.control.mousePosition({
+	position: 'bottomright', 
+	lngFormatter: function(num) {
+		var direction = (num < 0) ? 'O' : 'E';
+		return deg_to_dms(Math.abs(num)) + direction; 
+	},
+	latFormatter: function(num) {
+		var direction = (num < 0) ? 'S' : 'N';
+		return deg_to_dms(Math.abs(num)) + direction; 
+	},
+	separator: '  ',
+	emptyString: '&nbsp;'
+}).addTo(mapa);
 
 // Leaflet-MiniMap plugin https://github.com/Norkart/Leaflet-MiniMap
 var miniArgenmap = new L.TileLayer(argenmap._url, { minZoom: 0, maxZoom: 13, attribution: atrib_ign, tms: true });
