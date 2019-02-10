087766bb7d00dca6b4793a38a5456b4409abcf43
daf111 <daf111@yahoo.com.ar>
2019-02-10 12:13:36 -0300

    Origenes ilimitados
    Se implementó la posibilidad de incluir cantidad ilimitada de origenes en una clase.

--- js/entities.js
+++ js/entities.js
@@ -337,20 +337,21 @@ class GestorMenu {
 		this.plugins = {};
 		this.pluginsCount = 0;
 		this.pluginsLoading = 0;
+        
+		this._existsIndex = 0; //Identificador para evitar repetir ID de los items cuando provinen de distintas fuentes
 	}
 	
 	add(itemGroup) {
 		var itemAux;
-        var existsIndex = 0; //Si ya existía la sección y se desea adicionar capas
 		if (!this.items[itemGroup.seccion] || itemGroup.isBaseLayer()) { //itemGroup.isBaseLayer() avoid to repeat base layer into selector
 			itemAux = itemGroup;
 		} else {
 			itemAux = this.items[itemGroup.seccion];
-            existsIndex += 10000;
+            this._existsIndex = Object.keys(itemAux.itemsComposite).length + 1; //Si ya existe el itemGroup pero se agregan datos de otras fuentes, esto evita que se repitan los ID
 		}
 		for (var key in itemGroup.itemsComposite) {
-            if (existsIndex > 0) { //Para modificar item.seccion para no duplicar el contenido
-                itemGroup.itemsComposite[key].seccion += existsIndex;
+            if (this._existsIndex > 0) { //Para modificar item.seccion para no duplicar el contenido
+                itemGroup.itemsComposite[key].seccion += this._existsIndex;
             }
 			itemAux.setItem(itemGroup.itemsComposite[key]);
 		}
