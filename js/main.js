ebf913f2fe8c91a1dc3287d5ce6b3aeea6878a89
Gonzalo Gabriel Perez <zalitoar@gmail.com>
2020-08-21 11:32:11 -0300

    addLayers & changeProfile

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ const impresorItemCapaBase = new ImpresorItemCapaBaseHTML(),
   impresorGroupWMSSelector = new ImpresorGroupWMSSelector(),
   app = {
     profile: "default",
-    profiles: null,
+    profiles: {},
     templates: [
       "ign-geoportal-basic",
       "ign-geoportal-minimal"
@@ -32,61 +32,15 @@ const impresorItemCapaBase = new ImpresorItemCapaBaseHTML(),
 
     saveConfig: function () {
       this._save(app)
-    }
+    },
 
-  }
+    addBasemaps: function () {
 
-let getGeoserverCounter = 0,
-  keywordFilter = 'dato-basico-y-fundamental',
-  template = "",
-  templateFeatureInfoFieldException = [],
-  gestorMenu = new GestorMenu();
+      app.items.forEach(element => {
 
-$.getJSON("./js/menu.json", function (data) {
-
-  app.init(data);
-
-  //Template
-  template = app.template; // define wich template to use
-  gestorMenu.setLegendImgPath('templates/' + template + '/img/legends/');
-  delete data['template']; // delete template item from data
-
-  //templateFeatureInfoFieldException
-  if (app.template_feature_info_exception) {
-    templateFeatureInfoFieldException = app.template_feature_info_exception; // define not showing fields in feature info popup
-    delete data['template_feature_info_exception']; // delete template item from data
-  }
-
-  //Layers Joins (join several layers into one item)
-  if (app.layers_joins) {
-    gestorMenu.setLayersJoin(app.layers_joins);
-    delete data['layers_joins']; // delete template item from data
-  }
-
-  //Folders (generate folders items into main menu to generate logical groups of layers)
-  if (app.folders) {
-    gestorMenu.setFolders(app.folders);
-    delete data['folders']; // delete folders item from data
-  }
-
-  $.each(data, function (key, val) {
-    // replacing this data and previous for app object duplicates layers
-
-    for (let key in app.items) {
-
-      let item = app.items[key],
-        tab = new Tab(item.tab),
-        customizedLayers = (item.customize_layers == undefined) ? "" : item.customize_layers,
-        featureInfoFormat = (item.feature_info_format == undefined) ? "application/json" : item.feature_info_format,
-        impresorGroupTemp = impresorGroup;
-
-      if (item.tab == undefined) {
-        item.tab = "";
-      }
-
-      switch (item.type) {
-        case "basemap":
-          groupAux = new ItemGroupBaseMap(tab, item.nombre, item.seccion, item.peso, "", "", item.short_abstract, null);
+        if (element.type == "basemap") {
+          let item = element, tab = new Tab(item.tab),
+            groupAux = new ItemGroupBaseMap(tab, item.nombre, item.seccion, item.peso, "", "", item.short_abstract, null);
           groupAux.setImpresor(impresorBaseMap);
           groupAux.setObjDom(".basemap-selector");
           for (let key2 in item.capas) {
@@ -126,49 +80,128 @@ $.getJSON("./js/menu.json", function (data) {
           }
           gestorMenu.addTab(tab);
           gestorMenu.addItemGroup(groupAux);
-          break;
-        case "wmslayer":
-          getGeoserverCounter++;
-          if (tab.listType == "combobox") {
-            impresorGroupTemp = impresorGroupWMSSelector;
+        }
+      });
+    },
+
+    addLayers: function () {
+
+      app.items.forEach(element => {
+
+        if (element.type != "basemap") {
+          let item = element, tab = new Tab(item.tab),
+            customizedLayers = (item.customize_layers == undefined) ? "" : item.customize_layers,
+            featureInfoFormat = (item.feature_info_format == undefined) ? "application/json" : item.feature_info_format,
+            impresorGroupTemp = impresorGroup;
+
+          // Process item if is in profile
+          let matchItemProfile;
+          matchItemProfile = app.profiles[app.profile].data.find(e => e == item.id);
+          if (matchItemProfile != undefined) {
+            // matchItemProfile;
+            if (item.tab == undefined) {
+              item.tab = "";
+            }
+
+            switch (item.type) {
+              case "wmslayer":
+                getGeoserverCounter++;
+                if (tab.listType == "combobox") {
+                  impresorGroupTemp = impresorGroupWMSSelector;
+                }
+                let wmsLayerInfo = new LayersInfoWMS(item.host, item.servicio, item.version, tab, item.seccion, item.peso, item.nombre, item.short_abstract, featureInfoFormat, item.type, customizedLayers, impresorGroupTemp);
+                if (item.allowed_layers) {
+                  wmsLayerInfo.setAllowebLayers(item.allowed_layers);
+                }
+                if (item.customize_layers) {
+                  wmsLayerInfo.setCustomizedLayers(item.customize_layers);
+                }
+                gestorMenu.addTab(tab);
+                gestorMenu.addLayersInfo(wmsLayerInfo);
+                if (item.folders) {
+                  gestorMenu.addFolders(item.seccion, item.folders);
+                }
+                break;
+              case "wmts":
+                getGeoserverCounter++;
+                if (tab.listType == "combobox") {
+                  impresorGroupTemp = impresorGroupWMSSelector;
+                }
+                let wmtsLayerInfo = new LayersInfoWMTS(item.host, item.servicio, item.version, tab, item.seccion, item.peso, item.nombre, item.short_abstract, featureInfoFormat, item.type, customizedLayers, impresorGroupTemp);
+                if (item.allowed_layers) {
+                  wmtsLayerInfo.setAllowebLayers(item.allowed_layers);
+                }
+                if (item.customize_layers) {
+                  wmtsLayerInfo.setCustomizedLayers(item.customize_layers);
+                }
+                gestorMenu.addTab(tab);
+                gestorMenu.addLayersInfo(wmtsLayerInfo);
+                if (item.folders) {
+                  gestorMenu.addFolders(item.seccion, item.folders);
+                }
+                break;
+              default:
+                let sourceTypeUndefined = "The 'type' parameter is not set for the source:" + item.host;
+                console.log(sourceTypeUndefined);
+            }
+          } else {
+            // console.log("Profile or item id not defined")
           }
-          let wmsLayerInfo = new LayersInfoWMS(item.host, item.servicio, item.version, tab, item.seccion, item.peso, item.nombre, item.short_abstract, featureInfoFormat, item.type, customizedLayers, impresorGroupTemp);
-          if (item.allowed_layers) {
-            wmsLayerInfo.setAllowebLayers(item.allowed_layers);
-          }
-          if (item.customize_layers) {
-            wmsLayerInfo.setCustomizedLayers(item.customize_layers);
-          }
-          gestorMenu.addTab(tab);
-          gestorMenu.addLayersInfo(wmsLayerInfo);
-          if (item.folders) {
-            gestorMenu.addFolders(item.seccion, item.folders);
-          }
-          break;
-        case "wmts":
-          getGeoserverCounter++;
-          if (tab.listType == "combobox") {
-            impresorGroupTemp = impresorGroupWMSSelector;
-          }
-          let wmtsLayerInfo = new LayersInfoWMTS(item.host, item.servicio, item.version, tab, item.seccion, item.peso, item.nombre, item.short_abstract, featureInfoFormat, item.type, customizedLayers, impresorGroupTemp);
-          if (item.allowed_layers) {
-            wmtsLayerInfo.setAllowebLayers(item.allowed_layers);
-          }
-          if (item.customize_layers) {
-            wmtsLayerInfo.setCustomizedLayers(item.customize_layers);
-          }
-          gestorMenu.addTab(tab);
-          gestorMenu.addLayersInfo(wmtsLayerInfo);
-          if (item.folders) {
-            gestorMenu.addFolders(item.seccion, item.folders);
-          }
-          break;
-        default:
-          let sourceTypeUndefined = "The 'type' parameter is not set for the source:" + item.host;
-          console.log(sourceTypeUndefined);
+        }
+      });
+
+    },
+
+    changeProfile: function (profile) {
+      try {
+        if (profile != undefined && app.profiles[profile] != undefined) {
+          app.profile = profile;
+        } else {
+          let message = `Profile parameter missing or not present in profiles property. Available profiles: ${Object.keys(app.profiles)}`;
+          return message;
+        }
+      } catch (error) {
+        return error;
       }
     }
-  });
+
+  }
+
+let getGeoserverCounter = 0,
+  keywordFilter = 'dato-basico-y-fundamental',
+  template = "",
+  templateFeatureInfoFieldException = [],
+  gestorMenu = new GestorMenu();
+
+$.getJSON("./js/menu.json", function (data) {
+  app.init(data);
+
+  //Template
+  template = app.template; // define wich template to use
+  gestorMenu.setLegendImgPath('templates/' + template + '/img/legends/');
+  delete app['template']; // delete template item from data
+
+  //templateFeatureInfoFieldException
+  if (app.template_feature_info_exception) {
+    templateFeatureInfoFieldException = app.template_feature_info_exception; // define not showing fields in feature info popup
+    delete app['template_feature_info_exception']; // delete template item from data
+  }
+
+  //Layers Joins (join several layers into one item)
+  if (app.layers_joins) {
+    gestorMenu.setLayersJoin(app.layers_joins);
+    delete app['layers_joins']; // delete template item from data
+  }
+
+  //Folders (generate folders items into main menu to generate logical groups of layers)
+  if (app.folders) {
+    gestorMenu.setFolders(app.folders);
+    delete app['folders']; // delete folders item from data
+  }
+
+  app.addBasemaps();
+  app.addLayers();
+
   template = 'templates/' + template + '/main.html';
   $('#template').load(template);
 });
