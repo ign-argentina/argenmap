92f1a6c11c5cd49a9a4b7b1593982790516a9222
Damian Lopez <damian.lopez.041995@gmail.com>
2021-01-18 11:45:53 -0300

    Función que genera popup para layers ahora se ejecuta desde argenmap

--- js/functions.js
+++ js/functions.js
@@ -1,7 +1,13 @@
 // -- Plugins Control
 var plugins = new Array("loadGeojson", "loadWms");
 
-function deg_to_dms (deg) {
+var loadTableAsPopUp = false;
+
+function setTableAsPopUp(cond) {
+    loadTableAsPopUp = cond;
+}
+
+function deg_to_dms(deg) {
    var d = Math.floor (deg);
    var minfloat = (deg-d)*60;
    var m = Math.floor(minfloat);
@@ -49,7 +55,7 @@ function sanatizeString(s) {
 }
 
 /****** Enveloped functions ******/
-function loadGeojson (url, layer) {
+function loadGeojson(url, layer) {
     if (typeof loadGeojsonTpl === 'function') {
         return loadGeojsonTpl(wmsUrl, layer);
     } else {
@@ -57,23 +63,137 @@ function loadGeojson (url, layer) {
     }
 }
 
-function loadWms (callbackFunction, objLayer) {
+function loadWmsTplAux(objLayer, param) {
+    wmsUrl = objLayer.capa.host;
+		layer = objLayer.capa.nombre;
+    if (overlayMaps.hasOwnProperty(layer)) {
+        overlayMaps[layer].removeFrom(mapa);
+        delete overlayMaps[layer];
+    } else {
+        createWmsLayer(objLayer);
+        overlayMaps[layer].addTo(mapa);
+    }
+    
+    function ucwords (str) {
+        return str.charAt(0).toUpperCase() + str.slice(1);
+    }
+
+    //Parse FeatureInfo to display into popup (if info is text/html)
+    function parseFeatureInfoHTML(info, idTxt) {
+        infoAux = info.search("<ul>"); // search if info has a list
+        if (infoAux > 0) { // check if info has any content, if so shows popup
+            $(info).find('li').each(function( index ) {
+                var aux = $( this ).text().split(':');
+                info = info.replace('<b>' + aux[0] + '</b>:', '<b>' + ucwords(aux[0].replace(/_/g, ' ')) + ':</b>');
+            });
+            
+            info = info.replace('class="featureInfo"', 'class="featureInfo" id="featureInfoPopup' + idTxt + '"');
+            
+            return info;
+        } else {
+            infoAux = info.search("<table"); // search if info has a table
+            if (infoAux > 0) { // check if info has any content, if so shows popup
+                info = info.replace('<table', '<table class="featureInfo" id="featureInfoPopup' + idTxt + '"');
+                return info;
+            }
+        }
+        return '';
+    }
+    
+    //Parse FeatureInfo to display into popup (if info is application/json)
+    function parseFeatureInfoJSON(info, idTxt, title) {
+        info = JSON.parse(info);
+        if (info.features.length > 0) { // check if info has any content, if so shows popup
+            
+            var infoAux = '<div class="featureInfo" id="featureInfoPopup' + idTxt + '">';
+            infoAux += '<div class="featureGroup">';
+            infoAux += '<div style="padding:1em" class="individualFeature">';
+            infoAux += '<h4 style="border-top:1px solid gray;text-decoration:underline;margin:1em 0">' + title + '</h4>';
+            infoAux += '<ul>';
+            
+            for (i in info.features) {
+                Object.keys(info.features[i].properties).forEach(function(k){
+                    if (k != 'bbox') { //Do not show bbox property
+                        infoAux += '<li>';
+                        infoAux += '<b>' + ucwords(k.replace(/_/g, ' ')) + ':</b>';
+                        if (info.features[i].properties[k] != null) {
+                            infoAux += ' ' + info.features[i].properties[k];
+                        }
+                        infoAux += '<li>';
+                    }
+                });
+            }
+            
+            infoAux += '</ul>';
+            infoAux += '</div></div></div>';
+            
+            return infoAux;
+        }
+        
+        return '';
+    }
+    
+    function createWmsLayer(objLayer) {
+        //Extends WMS.Source to customize popup behavior
+        var MySource = L.WMS.Source.extend({
+            'showFeatureInfo': function(latlng, info) {
+				console.log(this, '...')
+                if (!this._map) {
+                    return;
+				}
+                
+                if (!loadTableAsPopUp) {
+                    if (this.options.INFO_FORMAT == 'text/html') {
+                        var infoParsed = parseFeatureInfoHTML(info, popupInfo.length);
+                    } else {
+                        var infoParsed = parseFeatureInfoJSON(info, popupInfo.length, this.options.title);
+                    }
+                    if (infoParsed != '') { // check if info has any content, if so shows popup
+                        var popupContent = $('.leaflet-popup').html();
+                        popupInfo.push(infoParsed); //First info for popup
+                    }
+                    if (popupInfo.length > 0) {
+                        popupInfoToPaginate = popupInfo.slice();
+                        latlngTmp = latlng;
+                        this._map.openPopup(paginateFeatureInfo(popupInfo, 0, false, true), latlng); //Show all info
+                        popupInfoPage = 0;
+                    }
+                } else {
+                    let tableD = new Datatable (JSON.parse(info), latlng);
+					createTabulator(tableD);
+                }
+                return;
+            }
+        });
+        var wmsSource = new MySource(objLayer.capa.getHostWMS(), {
+            transparent: true,
+            tiled: true,
+            maxZoom: 21,
+            'title': objLayer.capa.titulo,
+            format: 'image/png',
+            INFO_FORMAT: objLayer.capa.featureInfoFormat
+        });
+        overlayMaps[objLayer.capa.nombre] = wmsSource.getLayer(objLayer.capa.nombre);
+    }
+}
+
+function loadWms(callbackFunction, objLayer) {
     if (typeof callbackFunction === 'function') {
-        return callbackFunction(objLayer);
+        return loadWmsTplAux(objLayer, null);
     } else {
         console.warn("Function " + callbackFunction + "() do not exists. Please, define it.");
     }
 }
 
-function loadWmts (callbackFunction, objLayer) {
+function loadWmts(callbackFunction, objLayer) {
     if (typeof callbackFunction === 'function') {
-        return callbackFunction(objLayer);
+        return loadWmsTplAux(objLayer, null);
     } else {
         console.warn("Function " + callbackFunction + "() do not exists. Please, define it.");
     }
 }
 
-function loadMapaBase (tmsUrl, layer, attribution) {
+function loadMapaBase(tmsUrl, layer, attribution) {
     if (typeof loadMapaBaseTpl === 'function') {
         return loadMapaBaseTpl(tmsUrl, layer, attribution);
     } else {
@@ -81,7 +201,7 @@ function loadMapaBase (tmsUrl, layer, attribution) {
     }
 }
 
-function loadMapaBaseBing (bingKey, layer, attribution) {
+function loadMapaBaseBing(bingKey, layer, attribution) {
     if (typeof loadMapaBaseBingTpl === 'function') {
         return loadMapaBaseBingTpl(bingKey, layer, attribution);
     } else {
