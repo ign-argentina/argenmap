f3f5d4e7863a1d5da8359061bf5ca970ba1c56bc
Damian Lopez <damian.lopez.041995@gmail.com>
2021-02-12 12:42:49 -0300

    analytics es parte de argenmap, no es un plugin

--- plugins/analytics/analytics.js
+++
@@ -1,6 +0,0 @@
-$.getScript("https://www.googletagmanager.com/gtag/js?id=G-5L2LMQ1STF", function( data, textStatus, jqxhr ) {
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', 'G-5L2LMQ1STF');
-});
