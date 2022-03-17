2fba8e052db8c885e9982c64696e2325e0a52b4e
Lucas Vallejo <dday.developer@gmail.com>
2022-03-17 16:03:09 -0300

    Actualizacion de fecha de imagen y la opcion de que hay aqui?

--- src/js/entities.js
+++ src/js/entities.js
@@ -3097,3 +3097,119 @@ class Geometry {
   }
 }
 
+
+/******************************************
+CAPTURAR FECHA DE IMAGEN SATELITAL
+******************************************/
+class Fechaimagen {
+    constructor(lat,long,zoom) {
+        this.lat = lat;
+        this.long = long;
+        this.zoom = zoom;
+    }
+
+
+   get area() {
+     return this.getFechaImagen();
+   }
+
+    getFechaImagen() {
+        
+        var date_pic = "";
+
+       
+
+          var x = this.long * 20037508.34 / 180;
+          var y = Math.log(Math.tan((90 + parseFloat(this.lat)) * Math.PI / 360)) / (Math.PI / 180);
+          y = y * 20037508.34 / 180;
+          var id = "";
+            
+            if (this.zoom==19) {
+                id=9;
+            }else if(this.zoom==18){
+                id=10;
+            }else if(this.zoom==17){
+                id=11;
+            }
+            else if(this.zoom==16){
+                id=12;
+            }
+            else if(this.zoom==15){
+                id=13;
+            }
+            else if(this.zoom==14){
+                id=14;
+            }else if(this.zoom==13){
+                id=15;
+            }else if(this.zoom==12){
+                id=16;
+            }
+
+            $.get({
+              url: "https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/"+id+"/query?f=json&returnGeometry=true&spatialRel=esriSpatialRelIntersects&geometry=%7B%22xmin%22%3A"+x+"%2C%22ymin%22%3A"+y+"%2C%22xmax%22%3A"+x+"%2C%22ymax%22%3A"+y+"%2C%22spatialReference%22%3A%7B%22wkid%22%3A102100%2C%22latestWkid%22%3A3857%7D%7D&geometryType=esriGeometryEnvelope&inSR=102100&outFields=OBJECTID%2CSRC_DATE%2CSRC_RES%2CSRC_ACC%2CSAMP_RES%2CSRC_DESC%2CMinMapLevel%2CMaxMapLevel%2CNICE_NAME%2CDrawOrder%2CSRC_DATE2%2CNICE_DESC%2CShape_Length%2CShape_Area&outSR=102100",// mandatory
+               async:false ,
+              success: function (data) {
+                console.log(data)
+                 date_pic =new Date(data.features[0].attributes.SRC_DATE2)
+                  
+                }
+                
+             
+            });
+
+        return date_pic;
+          
+
+
+    }
+
+  
+
+}
+
+
+
+/******************************************
+CAPTURAR DIRECCION Y DATOS DE COORDENADAS
+******************************************/
+class QuehayAqui {
+    constructor(lat,long) {
+        this.lat = lat;
+        this.long = long;
+       
+    }
+
+
+   get area() {
+     return this.getQuehayAqui();
+   }
+
+    getQuehayAqui() {
+        
+        var quehay = "";
+
+       
+
+          var x = this.long * 20037508.34 / 180;
+          var y = Math.log(Math.tan((90 + parseFloat(this.lat)) * Math.PI / 360)) / (Math.PI / 180);
+          y = y * 20037508.34 / 180;
+          
+
+            $.get({
+              url: "https://api.ign.gob.ar/buscador/search?q=-32.2267,-67.7966&limit=5",
+               async:false ,// to make it synchronous
+              success: function (data) {
+                
+                 quehay = data[0].row_to_json.properties;
+                  
+                }
+                
+             
+            });
+
+        return quehay;
+          
+
+
+    }
+}
