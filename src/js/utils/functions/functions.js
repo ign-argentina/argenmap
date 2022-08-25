e8b160f886536db7568e058861b32c27006eb8b3
Zalitoar <zalitoar@gmail.com>
2022-08-25 08:37:01 -0300

    toggleVisibility function

--- src/js/utils/functions/functions.js
+++ src/js/utils/functions/functions.js
@@ -1019,3 +1019,14 @@ function hillShade() {
     }
 }
 }
+
+function toggleVisibility(elementId) {
+  try {
+    let el = document.getElementById(elementId);
+    el.classList.contains("visible")
+      ? (el.classList.remove("visible"), el.classList.add("hidden"))
+      : (el.classList.remove("hidden"), el.classList.add("visible"));
+  } catch (e) {
+    console.error(e);
+  }
+}
