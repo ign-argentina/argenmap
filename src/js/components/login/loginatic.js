040042b997a2387f9051178c59645ed3886aacf6
Gustavo S Coutinho <gscoutinho@ign.gob.ar>
2023-01-06 17:17:36 -0300

    fix: logOut Button style in firefox

--- src/js/components/login/loginatic.js
+++ src/js/components/login/loginatic.js
@@ -1,112 +1,115 @@
-loginatic = function() {
+loginatic = function () {
+  this.currentLogin = false;
+  this.notLoggedTxt =
+    app.login.notLogged || "Contraseña no válida, intente nuevamente";
+  this.noPassword = app.login.noPassword || "Campo vacío, ingrese su contraseña";
+  this.init = (conf) => {
+    if (getCookie("autologin") == null) {
+      setCookie("autologin", 0);
+    }
+    if (getCookie("autologin") == "1") {
+      this._addLogoutButton();
+    }
+  };
 
-    this.currentLogin = false;
-    this.notLoggedTxt = app.login.notLogged || "Password not found, please retry with a valid one.";
-    this.noPassword =  app.login.noPassword || "No password given!";
-    this.init = (conf) => {
-        if (getCookie("autologin") == null) {
-            setCookie("autologin", 0);
+  this.check = () => {
+    //setCookie("autologin", 0);
+    if (getCookie("autologin") == 1) {
+      let lat = getCookie("lat");
+      let lon = getCookie("lon");
+      let zoom = getCookie("zoom");
+
+      setTimeout(function () {
+        if (mapa.setView) {
+          mapa.setView([lat, lon], zoom);
         }
-        if (getCookie("autologin") == "1") {
-            this._addLogoutButton();
-        }
+      }, 500);
+    } else {
+      document.getElementById("login-wrapper").style.display = "flex";
+    }
+  };
+
+  this.process = async () => {
+    let pwd = document.getElementById("input-pwd").value;
+
+    if (!pwd.length) {
+      alert(this.noPassword);
+      // new UserMessage(this.noPassword, true, 'error');
     }
 
-    this.check = () => {
-        //setCookie("autologin", 0);
-        if (getCookie("autologin") == 1) {
-            let lat = getCookie("lat");
-            let lon = getCookie("lon");
-            let zoom = getCookie("zoom");
-
-            setTimeout(function() {
-                if (mapa.setView) { mapa.setView([lat, lon], zoom); }
-
-            }, 500);
-        } else {
-            document.getElementById("login-wrapper").style.display = "flex";
+    if (pwd.length) {
+      //let result = await fetch("src/config/user.json")
+      let notLoggedTxt = this.notLoggedTxt;
+      let result = await fetch(app.login.api + pwd).then(function (response) {
+        if (response.status >= 500) {
+          new UserMessage(
+            "Looks like there was a problem. Status Code: " + response.status,
+            true,
+            "error"
+          );
+          return;
         }
-    }
-
-    this.process = async() => {
-        let pwd = document.getElementById("input-pwd").value;
-
-        if( !pwd.length ) {
-            alert(this.noPassword);
-            // new UserMessage(this.noPassword, true, 'error');
+        if (response.status !== 200) {
+          alert(notLoggedTxt);
+          return;
         }
+        if (response.status === 200) {
+          return response.json();
+        }
+      });
+      let logged = false;
 
-        if( pwd.length ) {
-            //let result = await fetch("src/config/user.json")
-            let notLoggedTxt = this.notLoggedTxt;
-            let result = await fetch( app.login.api + pwd )
-            .then(function (response) {
-                if ( response.status >= 500 ) {
-                    new UserMessage('Looks like there was a problem. Status Code: ' + response.status, true, 'error');
-                    return;
-                }
-                if ( response.status !== 200 ) {
-                    alert(notLoggedTxt);
-                    return;
-                }
-                if ( response.status === 200 ) {
-                    return response.json();
-                }
-    
-            });
-            let logged = false;
-    
-            /* if (result.clave == pwd) { */
-            if (result.clave) {
-                let recuerdame = $("#inp-recuerdame").prop("checked") ? 1 : 0;
-    
-                this.currentLogin = result;
-    
-                let lat = result.lat_4326;
-                let lon = result.lon_4326;
-                let zoom = result.zoom ?? 13;
-    
-                /* document.title += ' - ' + json[i].nombregobiernolocal;
+      /* if (result.clave == pwd) { */
+      if (result.clave) {
+        let recuerdame = $("#inp-recuerdame").prop("checked") ? 1 : 0;
+
+        this.currentLogin = result;
+
+        let lat = result.lat_4326;
+        let lon = result.lon_4326;
+        let zoom = result.zoom ?? 13;
+
+        /* document.title += ' - ' + json[i].nombregobiernolocal;
                 let logoTitle = document.getElementById('logoText');
                 logoTitle.innerText += ' - ' + json[i].nombregobiernolocal; */
-    
-                mapa.setView([lat, lon], zoom);
-                document.getElementById("login-wrapper").style.display = "none";
-                logged = true;
-    
-                if ($("#btn-logout").length == 0) {
-                    this._addLogoutButton();                    
-                }
-    
-                if (recuerdame == 1) {
-                    setCookie("lat", lat);
-                    setCookie("lon", lon);
-                    setCookie("zoom", 10);
-                    setCookie("autologin", 1);
-                }
-            }
-    
-            if (!logged) {
-                alert(this.notLoggedTxt);
-            }
-        }
-    }
 
-    this.logout = () => {
-        this.currentLogin = false;
-        setCookie("autologin", 0);
-        /* 
+        mapa.setView([lat, lon], zoom);
+        document.getElementById("login-wrapper").style.display = "none";
+        logged = true;
+
+        if ($("#btn-logout").length == 0) {
+          this._addLogoutButton();
+        }
+
+        if (recuerdame == 1) {
+          setCookie("lat", lat);
+          setCookie("lon", lon);
+          setCookie("zoom", 10);
+          setCookie("autologin", 1);
+        }
+      }
+
+      if (!logged) {
+        alert(this.notLoggedTxt);
+      }
+    }
+  };
+
+  this.logout = () => {
+    this.currentLogin = false;
+    setCookie("autologin", 0);
+    /* 
         let lat = -40;
         let lon = -59;
         let zoom = 4; 
         mapa.setView([lat, lon], zoom);
         */
-        mapa.resetView();
-        location.reload();
-    }
+    mapa.resetView();
+    location.reload();
+  };
 
-    this._addLoginWrapper = () => {
-        const wrapperHtml = `
+  this._addLoginWrapper = () => {
+    const wrapperHtml = `
             <div class="container-fluid col-12 col-xs-12 col-sm-6 col-md-3 mt-5 text-center" style="display: flex;align-items: center;">
                 <div class="login">
                     <img src="src/styles/images/lupa.png" width="10%">
@@ -140,11 +143,21 @@ loginatic = function() {
                 </div>
             </div>
         `;
-        $("#login-wrapper").append(wrapperHtml);
-    }
+    document.getElementById("login-wrapper").append(wrapperHtml);
+  };
 
-    this._addLogoutButton = () => {
-        $("#mapa").append(`<div class="center-flex btn-logout" id="btn-logout" title="cerrar sesión" onclick="loginatic.logout();" style="width: 30px; height: 30px; border: medium none; box-shadow: rgba(0, 0, 0, 0.65) 0px 1px 5px;"><div class="center-flex" id="icon-container"><span class="fa fa-sign-out-alt" aria-hidden="true"></span></div></div>`);
-    }
+  this._addLogoutButton = () => {
+    const logoutButton = document.createElement("div");
+    logoutButton.className = "center-flex btn-logout";
+    logoutButton.id = "btn-logout";
+    logoutButton.title = "Cerrar sesión";
+    logoutButton.onclick = function () {
+      loginatic.logout();
+    };
+    logoutButton.innerHTML = `<div class="center-flex" id="icon-container"><span class="fa fa-sign-out-alt" aria-hidden="true"></span></div>`;
 
-}
+    document.getElementById("mapa").append(logoutButton);
+
+    // $("#mapa").append(`<div class="center-flex btn-logout" id="btn-logout" title="Cerrar sesión" onclick="loginatic.logout();" style="width: 30px; height: 30px; border: medium none; box-shadow: rgba(0, 0, 0, 0.65) 0px 1px 5px;"><div class="center-flex" id="icon-container"><span class="fa fa-sign-out-alt" aria-hidden="true"></span></div></div>`);
+  };
+};
