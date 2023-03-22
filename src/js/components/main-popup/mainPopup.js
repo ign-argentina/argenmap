05309c1312056b716bd5788f5e08998b85ae2c29
afcirillo <agustincirillo@hotmail.com>
2023-03-22 15:25:08 -0300

    branch mainPopup created

---
+++ src/js/components/main-popup/mainPopup.js
@@ -0,0 +1,50 @@
+mainPopup = function () {
+  //FINAL
+  this._addPopupWrapper = () => {
+    const mainPopup = document.createElement("div");
+    mainPopup.id="main-popup";
+    mainPopup.classList = "justify-content-center";
+    document.body.append(mainPopup);
+
+    const mainWrapper = document.createElement("div");
+    mainWrapper.id="mainWrapper";
+    mainWrapper.style="display: flex;align-items: center;"
+    mainWrapper.classList = "container-fluid col-12 col-xs-12 col-sm-6 col-md-3 mt-5 text-center";
+    $("#main-popup").append(mainWrapper);
+    
+    const contentWrapper = document.createElement("div");
+    contentWrapper.id="contentWrapper";
+    contentWrapper.classList = "popup";
+    contentWrapper.innerHTML = '<h4>Bienvenido!</h4><img src="src/styles/images/lupa.png" width="10%">';
+    mainWrapper.appendChild(contentWrapper);
+    
+    let btncloseWrapper = document.createElement("a");
+    btncloseWrapper.id = "btnclose-wrapper";
+    btncloseWrapper.href = "javascript:void(0)";
+    btncloseWrapper.style = "float:right; color:#676767; overflow-y:auto;";
+    btncloseWrapper.innerHTML ='<i class="fa fa-times"></i>';
+    btncloseWrapper.onclick = () => {
+      mainWrapper.remove();
+      document.getElementById("main-popup").style.display = "none";
+    };
+    contentWrapper.append(btncloseWrapper);
+
+    let checkbox = document.createElement("div");
+    checkbox.id="contentWrapper";
+    checkbox.classList = "checkbox";
+    checkbox.innerHTML ='<label><input type="checkbox" id="popupCheckbox" name="popupCheckbox">No volver a mostrar</label>';
+    contentWrapper.appendChild(checkbox); 
+
+    let check = document.querySelector("input[name=popupCheckbox]");
+    check.addEventListener('change', function() {
+      if (this.checked) {
+        console.log("Checkbox is checked..");
+      } else {
+        console.log("Checkbox is not checked..");
+      }
+    });
+  };
+
+  
+
+};
