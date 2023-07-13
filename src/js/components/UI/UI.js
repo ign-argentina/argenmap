665bac608eb88f1d5bc08f1445553144d40df443
GSC996 <gscoutinho@outlook.com>
2023-07-13 15:30:14 -0300

    refactor: add UI class

---
+++ src/js/components/UI/UI.js
@@ -0,0 +1,23 @@
+class UI {
+    constructor() {
+        
+    }
+}
+
+class AboutUsModal extends UI{
+    constructor() {
+        
+    }
+}
+
+class AboutUsTab extends UI{
+    constructor() {
+        
+    }
+}
+
+class NotificationPoint extends UI{
+    constructor() {
+        
+    }
+}
