a06d67bdb74d7c458f278ea427578ac73d22e168
afcirillo <agustincirillo@hotmail.com>
2023-09-06 16:25:55 -0300

    layer classes added to module

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,14 @@
 import * as modules from './modules/modules.js';
 
 // instance of BaseLayer class
-let argenmapLayer = new modules.newmap.BaseLayer();
+//let argenmapLayer = new modules.newmap.BaseLayer();
 
 // accessing baselayer method
-console.log(argenmapLayer.name);
+//console.log(argenmapLayer.name);
+
+let TESTargenmapLayer = new modules.newmap.Raster();
+TESTargenmapLayer.type = "soy raster";
+TESTargenmapLayer.setOpacity("mucha opacidad")
+console.log(TESTargenmapLayer);
 
 // let contourLines = modules.process.Contour(); 
--- src/js/modules/map/map.js
+++ src/js/modules/map/map.js
@@ -1,20 +1,81 @@
 class Layer {
     constructor(id, name, title, attribution, icon, weight, keywords) {
         this.id = id,
-            this.name = name ?? "A sample layer",
-            this.title = title,
-            this.attribution = attribution,
-            this.icon = icon,
-            this.weight = weight,
-            this.keywords = keywords
+        this.name = name ?? "A sample layer",
+        this.title = title,
+        this.attribution = attribution,
+        this.icon = icon,
+        this.weight = weight,
+        this.keywords = keywords
     }
-
+    
     addTo() { };
     remove() { };
     removeFrom() { };
     isActive() { };
 }
 
+//VECTOR--------------------
+export class Vector extends Layer {
+    constructor(type) {
+        super();
+        this.type = type;
+    }
+}
+
+class VectorFile extends Vector {
+    constructor() {
+        super();
+    }
+}
+
+class WFSLayer extends Vector {
+    constructor() {
+        super();
+    }
+}
+
+
+
+//RASTER--------------------
+export class Raster extends Layer {
+    constructor(type) {
+        super();
+        this.type = type;
+        this.opacity = null;
+    }
+
+    setOpacity(opacity) {
+        this.opacity = opacity;
+    }
+}
+class TileLayer extends Raster {
+    constructor() {
+        super();
+    }
+}
+class RasterFile extends Raster {
+    constructor() {
+        super();
+    }
+}
+class WMTSLayer extends Raster {
+    constructor() {
+        super();
+    }
+}
+class WMSLayer extends Raster {
+    constructor() {
+        super();
+    }
+
+    get featureInfo() {
+    }
+}
+
+
+
+//BASE LAYER-----------------
 export class BaseLayer extends Layer {
     constructor(url, theme) {
         super();
