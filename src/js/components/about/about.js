96ee8e72ce59021a99221be22aef25f8c5c2041a
InMunken <Federico.InMunken@gmail.com>
2023-10-12 11:49:16 -0300

    Refactor: mooved aboutUs to about.js

--- src/js/components/about/about.js
+++ src/js/components/about/about.js
@@ -285,6 +285,215 @@ class AboutUs {
     }
 }
 
+class AboutUsModal extends UIComponent {
+    constructor() {
+      super();
+    }
+  
+    /**
+     * Creates the About Us modal element and appends it to the document body.
+     * @param {Array} tabs - The array of tab objects.
+     */
+    createElement(tabs) {
+      const principalContainer = document.createElement("div");
+      principalContainer.id = "whole-about";
+  
+      const aboutHeader = document.createElement("div");
+      aboutHeader.className = "about-header";
+  
+      const aboutLogo = document.createElement("img");
+      aboutLogo.src = "src/styles/images/argenmap-banner.webp";
+      aboutLogo.className = "about-logo";
+  
+      const aboutExitBtn = document.createElement("a");
+      aboutExitBtn.id = "aboutExitBtn";
+      aboutExitBtn.classList = "about-exit";
+      aboutExitBtn.innerHTML = '<i class="fa fa-times"></i>';
+      aboutExitBtn.onclick = () => {
+        const notiDots = document.querySelectorAll(".notification-dot");
+        notiDots.forEach((dot) => { dot.remove() });
+        principalContainer.remove();
+        this.isVisible = false;
+      };
+  
+      const aboutMainSection = document.createElement("div");
+      aboutMainSection.className = "about-main-section";
+  
+      const aboutTabsContainer = document.createElement("div");
+      aboutTabsContainer.className = "about-tabs-bar";
+  
+      aboutHeader.appendChild(aboutLogo);
+      aboutHeader.appendChild(aboutExitBtn);
+  
+      principalContainer.appendChild(aboutHeader);
+      aboutMainSection.appendChild(aboutTabsContainer);
+      principalContainer.appendChild(aboutMainSection);
+  
+      document.body.appendChild(principalContainer);
+  
+      tabs.forEach((tab, i) => {
+        const tabItem = new AboutUsTab();
+        tabItem.createElement(tab, i);
+      });
+  
+      const tabContent = new AboutUsTab();
+  
+      const readmeContainer = tabContent.createReadmeContainer();
+      const functionContainer = tabContent.createFunctionsContainer();
+      //const contributorContainer = tabContent.createContributorsContainer();
+  
+      aboutMainSection.appendChild(readmeContainer);
+      aboutMainSection.appendChild(functionContainer);
+      //aboutMainSection.appendChild(contributorContainer);
+  
+      const img = new Imagen("hello", batman_URL, "texto alternativo", "especificamenteparaesto", "batman besto hero ever")
+  
+      
+      const tabElement = new TabElement("TabDePrueba", "idtab", "tab", function () {
+        modalAboutUs.showTab(1);
+      });
+      
+      const options = [
+        {
+          label: 'Option 1',
+          action: () => {
+            console.log('JEJE');
+          },
+        },
+        {
+          label: 'Option 2',
+          action: () => {
+            console.log('JAJA');
+          },
+        },
+      ];
+      
+      //const mapItem = new BaseMapItem('IntentoMap!', batman_URL, 'map-item-button', options);
+      
+      /* const input = new Input("aynidid", "no-clase", "color")
+      
+      // Crear un elemento de tipo color picker
+      const textInput = new InputText('textID', 'text-input-class', 'Placeholder     ');
+      // Crear un elemento de tipo checkbox con etiqueta personalizada
+      const checkbox = new Checkbox('checkboxInput', 'checkbox-input-class', 'Acepto los términos y condiciones');
+      
+      // Agregar los elementos al documento
+      checkbox.addTo("readme-container");
+      
+      tabElement.addTo("readme-container")
+      img.addTo("readme-container");
+      textInput.addTo("readme-container")
+      mapItem.addTo("readme-container");
+      
+      input.addTo("readme-container") */
+      const button = new Button("button-number-one", "hello-kitty", "Click me", function () {
+        console.log(colorInput.whatColor());
+      })
+      button.addTo("readme-container");
+      
+      const colorInput = new InputColor('colorInput', 'color-input-class');
+      colorInput.addTo("readme-container");
+  
+  
+      const buttoncolor = new Button("button-nuber-two", "hello-devil", "Change color to white", function (){
+        colorInput.changeValue('#ffffff');
+      })
+      buttoncolor.addTo("readme-container");
+    }
+  }
+  
+  /**
+   * Represents the About Us tab in the user interface.
+   * @extends UIComponent
+   */
+  class AboutUsTab extends UIComponent {
+    constructor() {
+      super();
+    }
+  
+    /**
+     * Creates and appends a tab element to the about-tabs-bar container.
+     * @param {Object} tab - The tab object containing name and id properties.
+     * @param {number} i - The index of the tab.
+     */
+    createElement(tab, i) {
+      const tabElement = document.createElement('div');
+      tabElement.classList.add('tab');
+  
+      if (tab.name) {
+        tabElement.innerHTML = tab.name;
+        tabElement.id = tab.id;
+      } else {
+        tabElement.innerHTML = "TODPN"; // Te Olvidaste De Ponerle Nombre
+      }
+  
+      tabElement.addEventListener('click', () => {
+        modalAboutUs.showTab(i);
+      });
+  
+      document.querySelector(".about-tabs-bar").appendChild(tabElement);
+    }
+  
+    /**
+     * Creates the readme container element.
+     * @returns {HTMLElement} - The created readme container element.
+     */
+    createReadmeContainer() {
+      const readmeContainer = document.createElement('div');
+      readmeContainer.classList.add('content-about-tab', 'content-about-deactivate', 'readme-container');
+      readmeContainer.id = "readme-container";
+  
+      const repoIndication = document.createElement("p");
+      repoIndication.textContent = "Repositorio en GitHub";
+      repoIndication.style.margin = "0";
+  
+      const gitHubMark = document.createElement("img");
+      gitHubMark.src = "src/styles/images/github-mark-white.png";
+      gitHubMark.alt = "GitHub Logo";
+      gitHubMark.style.width = "24px";
+      gitHubMark.style.margin = "0 5px";
+  
+      const repoDiv = document.createElement("div");
+      repoDiv.appendChild(gitHubMark);
+      repoDiv.appendChild(repoIndication);
+      repoDiv.style.textAlign = "center";
+      repoDiv.id = "link-to-repo";
+  
+      readmeContainer.appendChild(repoDiv);
+      return readmeContainer;
+    }
+  
+    /**
+     * Creates the functions container element.
+     * @returns {HTMLElement} - The created functions container element.
+     */
+    createFunctionsContainer() {
+      const functionsContainer = document.createElement('div');
+      functionsContainer.classList.add('content-about-tab', 'content-about-deactivate');
+      functionsContainer.style.overflow = "auto";
+      functionsContainer.id = "functions-container";
+      return functionsContainer;
+    }
+  
+    /**
+     * Creates the contributors container element.
+     * @returns {HTMLElement} - The created contributors container element.
+     
+    createContributorsContainer() {
+        const contributorContainer = document.createElement('div');
+        contributorContainer.classList.add('content-about-tab', 'contributor-container', 'content-about-deactivate');
+        contributorContainer.id = "contributors-container";
+   
+        return contributorContainer;
+    }*/
+  }
+
+
+
+
+
+
+
 const modalAboutUs = new AboutUs();
 const dataGetter = new DataGetter();
 
@@ -294,3 +503,5 @@ const dataGetter = new DataGetter();
     modalAboutUs.contributors = contributorsData;
 })();
 */
+
+
