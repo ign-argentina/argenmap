947cf2a6cad2dbdcd1481713367c58e488a3f886
InMunken <Federico.InMunken@gmail.com>
2023-10-18 11:05:29 -0300

    add: tabs.menu.logic

--- src/js/components/baseMapMenu/baseMapMenu.js
+++ src/js/components/baseMapMenu/baseMapMenu.js
@@ -1,16 +1,24 @@
 class MenuBaseMap {
-    cotructor() {
+    constructor() {
         this.isVisible = false;
-        console.log('hello im alive')
+
+        //Ids for this class sections:  
+        this.wholeSectionID  = 'whole-basemapmenu'
+        this.headerSectionID = 'header-basemapmenu'
+        this.tabSectionID    = 'tabs-basemapmenu'
+        this.mainSectionID   = 'main-basemapmenu'
+        this.footSectionID   = 'foot-basemapmenu'
+
+
+
     }
 
     toggleOpen() {
         if (this.isVisible == false) {
             this.createMenu();
-            this.showTab();
+            this.showTab(0);
 
-            this.isVisible = true
-
+            this.isVisible = true;
         } else {
 
         }
@@ -19,14 +27,14 @@ class MenuBaseMap {
 
     createMenu() {
         //1. create the main containter
-        const mainContainer = new Container('whole-basemapmenu', 'menuContainer');
+        const mainContainer = new Container(this.wholeSectionID, 'menuContainer');
         mainContainer.changeStyle('overflow', 'hidden') //just a demostration
         
         //2. create diferent sections
-        const header = new Container('header-basemapmenu', 'outLine');
-        const tabSection = new Container('tabs-basemapmenu', 'outLine');
-        const mainSection = new Container('main-basemapmenu', 'outLine');
-        const footSection = new Container('foot-basemapmenu', 'outLine');
+        const header = new Container(this.headerSectionID, 'outLine');
+        const tabSection = new Container(this.tabSectionID, 'outLine');
+        const mainSection = new Container(this.mainSectionID, 'outLine');
+        const footSection = new Container(this.footSectionID, 'outLine');
         
         mainContainer.addToElement(document.body);
 
@@ -44,9 +52,6 @@ class MenuBaseMap {
         const labelTitle =  new Label(null, null, "Argregar capa base");
         labelTitle.changeStyle('color', 'white')
         
-        /*const element = document.createElement('input')
-        element.type = "color"
-        document.getElementById('whole-basemapmenu').appendChild(element);*/
 
         //3.3. create foot
         const consultButton = new Button(null, null, '?', () =>{
@@ -56,61 +61,90 @@ class MenuBaseMap {
             mainContainer.remove();
             this.isVisible = false; 
         })
-        
         //4. create the diferent tabs
         const libraryTab = new TabElement('Libreía', null, 'tab', () =>{
-            this.createLibrary('main-basemapmenu')
+            this.showTab(0);
         })
         const fromUrlTab = new TabElement('desde Url', null, 'tab', () =>{
-            this.createFromUrl('main-basemapmenu')
+            this.showTab(1);
         })
         const colorTab = new TabElement('color', null, 'tab', () =>{
-            this.createColor('main-basemapmenu')
+            this.showTab(2);
         })
 
+        
         
         
         
         //5. Add sections to main container
-       // header.addTo('whole-basemapmenu');
-       header.addToObjet(mainContainer)
-
-
-        tabSection.addTo('whole-basemapmenu');
-        mainSection.addTo('whole-basemapmenu');
-        footSection.addTo('whole-basemapmenu');
+        // header.addTo('whole-basemapmenu');
+        header.addToObjet(mainContainer)
         
+        
+        tabSection.addTo(this.wholeSectionID);
+        mainSection.addTo(this.wholeSectionID);
+        footSection.addTo(this.wholeSectionID);
+        
+        this.createLibrary(this.mainSectionID)
+        this.createFromUrl(this.mainSectionID)
+        this.createColor(this.mainSectionID)
         //5.1 Add elements to sections
-        labelTitle.addTo('header-basemapmenu');
-        exitButton.addTo('header-basemapmenu');
+        labelTitle.addTo(this.headerSectionID);
+        exitButton.addTo(this.headerSectionID);
 
-        libraryTab.addTo('tabs-basemapmenu');
-        fromUrlTab.addTo('tabs-basemapmenu');
-        colorTab.addTo('tabs-basemapmenu');
+        libraryTab.addTo(this.tabSectionID);
+        fromUrlTab.addTo(this.tabSectionID);
+        colorTab.addTo(this.tabSectionID);
 
-        consultButton.addTo('foot-basemapmenu');
-        readyButton.addTo('foot-basemapmenu');
+        consultButton.addTo(this.footSectionID);
+        readyButton.addTo(this.footSectionID);
 
+        console.log(document.querySelectorAll('.section-container'))
+
+    }
+
+    showTab(indexToShow){
+
+        const containersInDisplay = document.querySelectorAll('.section-container')
+
+        containersInDisplay.forEach(el => el.classList.add('content-about-deactivate'));
+        containersInDisplay[indexToShow].classList.remove('content-about-deactivate');
+
+        const tabsToDisplay = document.querySelectorAll('.tab');
+        tabsToDisplay.forEach(el => el.classList.remove('tab-active'));
+        tabsToDisplay[indexToShow].classList.add('tab-active');
 
     }
 
     createLibrary(mainContainerId) {
         console.log("a beautifull library in this id: " + mainContainerId);
+        const container =  new Container(null, "section-container")
+        // const imagendeprueba = new Imagen(null, batman_URL, 'batman1', null, null)
+        // imagendeprueba.addToObjet(container)
 
-        const imagendeprueba = new Imagen(null, batman_URL, 'batman1', null, null)
-        console.log(imagendeprueba)
-        imagendeprueba.addTo(mainContainerId)
+
+
+
+
+
+
+        container.addTo(mainContainerId)
     }
     createFromUrl(mainContainerId) {
+
         console.log("a beautifull from url section in this id: " + mainContainerId)
+        const container =  new Container(null, "section-container")
         const imagendeprueba = new Imagen(null, 'https://media.tenor.com/RrXsGhXSBDUAAAAC/ok-thumbs-up.gif', 'batman1', null, null)
-        imagendeprueba.addTo(mainContainerId)
+        imagendeprueba.addToObjet(container)
+        container.addTo(mainContainerId)
 
     }
     createColor(mainContainerId) {
         console.log("a beautifull color section in this id: " + mainContainerId)
+        const container =  new Container(null, "section-container")
         const imagendeprueba = new Imagen(null, 'https://media.tenor.com/-Oo9YBTLsvkAAAAd/michael-myers-halloween.gif', 'batman1', null, null)
-        imagendeprueba.addTo(mainContainerId)
+        imagendeprueba.addToObjet(container)
+        container.addTo(mainContainerId)
     }
 }
 
