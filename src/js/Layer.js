660041085242619c8896e0eb5e0397b1e0e7d12c
afcirillo96 <agustincirillo@hotmail.com>
2023-11-15 11:17:29 -0300

    vectorLayer test added

--- src/js/Layer.js
+++ src/js/Layer.js
@@ -84,62 +84,68 @@ class VectorLayer extends OverlayLayer {
     exportLayer() {
         console.log("export")
     }
-    setStyle(style) { }
-    addProperty() { }
-    removeProperty() { }
+    setStyle(style) {
+        console.log("style added")
+    }
+    addProperty() {
+        console.log("property added")
+    }
+    removeProperty() {
+        console.log("property removed")
+    }
 }
 
-// class LayerStyle {
-//     constructor(options) {
-//         this.defaultStyle = {
-//             "icon": {
-//                 "options": {
-//                     "iconUrl": "src/styles/images/marker.png",
-//                     "shadowUrl": "src/styles/images/marker-shadow.png",
-//                     "popupAnchor": [0, -41],
-//                     "iconSize": [25, 41],
-//                     "iconAnchor": [12.5, 41]
-//                 }
-//             },
-//             "color": "#ff7800",
-//             "weight": "8",
-//             "opacity": "0.35",
-//             "fill": true,
-//             "fillColor": "#613583",
-//             "fillOpacity": "0.65",
-//             "clickable": true,
-//             "dashArray": "16",
-//             "lineJoin": "miter",
-//             "stroke": true,
-//             "clickable": true,
-//             "lineCap": "butt",
-//             "radius": 132352.86427154695,
-//             "metric": true,
-//             "zIndexOffset": 2000,
-//             "repeatMode": false
-//         },
-//             this.icon = options.icon ?? this.defaultStyle.icon,
-//             this.color = options.color ?? this.defaultStyle.color,
-//             this.weight = options.weight ?? this.defaultStyle.weight,
-//             this.opacity = options.opacity ?? this.defaultStyle.opacity,
-//             this.fill = options.fill ?? this.defaultStyle.fill,
-//             this.fillColor = options.fillColor ?? this.defaultStyle.fillColor,
-//             this.fillOpacity = options.fillOpacity ?? this.defaultStyle.fillOpacity,
-//             this.clickable = options.clickable ?? this.defaultStyle.clickable,
-//             this.dashArray = options.dashArray ?? this.defaultStyle.dashArray,
-//             this.lineJoin = options.lineJoin ?? this.defaultStyle.lineJoin,
-//             this.stroke = options.stroke ?? this.defaultStyle.stroke,
-//             this.clickable = options.clickable ?? this.defaultStyle.clickable,
-//             this.lineCap = options.lineCap ?? this.defaultStyle.lineCap,
-//             this.radius = options.radius ?? this.defaultStyle.radius,
-//             this.metric = options.metric ?? this.defaultStyle.metric,
-//             this.zIndexOffset = options.zIndexOffset ?? this.defaultStyle.zIndexOffset,
-//             this.repeatMode = options.repeatMode ?? this.defaultStyle.repeatMode
-//     }
-//     resetStyle() {
-//         this = Object.assign(this.defaultStyle);
-//     }
-// }
+class LayerStyle {
+    constructor(options) {
+        this.defaultStyle = {
+            "icon": {
+                "options": {
+                    "iconUrl": "src/styles/images/marker.png",
+                    "shadowUrl": "src/styles/images/marker-shadow.png",
+                    "popupAnchor": [0, -41],
+                    "iconSize": [25, 41],
+                    "iconAnchor": [12.5, 41]
+                }
+            },
+            "color": "#ff7800",
+            "weight": "8",
+            "opacity": "0.35",
+            "fill": true,
+            "fillColor": "#613583",
+            "fillOpacity": "0.65",
+            "clickable": true,
+            "dashArray": "16",
+            "lineJoin": "miter",
+            "stroke": true,
+            "clickable": true,
+            "lineCap": "butt",
+            "radius": 132352.86427154695,
+            "metric": true,
+            "zIndexOffset": 2000,
+            "repeatMode": false
+        },
+            this.icon = options.icon ?? this.defaultStyle.icon,
+            this.color = options.color ?? this.defaultStyle.color,
+            this.weight = options.weight ?? this.defaultStyle.weight,
+            this.opacity = options.opacity ?? this.defaultStyle.opacity,
+            this.fill = options.fill ?? this.defaultStyle.fill,
+            this.fillColor = options.fillColor ?? this.defaultStyle.fillColor,
+            this.fillOpacity = options.fillOpacity ?? this.defaultStyle.fillOpacity,
+            this.clickable = options.clickable ?? this.defaultStyle.clickable,
+            this.dashArray = options.dashArray ?? this.defaultStyle.dashArray,
+            this.lineJoin = options.lineJoin ?? this.defaultStyle.lineJoin,
+            this.stroke = options.stroke ?? this.defaultStyle.stroke,
+            this.clickable = options.clickable ?? this.defaultStyle.clickable,
+            this.lineCap = options.lineCap ?? this.defaultStyle.lineCap,
+            this.radius = options.radius ?? this.defaultStyle.radius,
+            this.metric = options.metric ?? this.defaultStyle.metric,
+            this.zIndexOffset = options.zIndexOffset ?? this.defaultStyle.zIndexOffset,
+            this.repeatMode = options.repeatMode ?? this.defaultStyle.repeatMode
+    }
+    resetStyle() {
+        // this = Object.assign(this.defaultStyle);
+    }
+}
 
 
 let bltest = new BaseLayer("theme1","url.com");
