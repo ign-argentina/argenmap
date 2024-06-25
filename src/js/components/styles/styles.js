4d86e61ed327fff350eefa6f2a2a32e2765d0cab
Gustavo Silvestre Coutinho <gscoutinho@outlook.com>
2024-06-25 19:25:15 -0300

    chore: Update searchbar and dashboard CSS styles

--- src/js/components/styles/styles.js
+++ src/js/components/styles/styles.js
@@ -1,15 +1,25 @@
 class StylesUI {
 
- createstyles(){
-  const style = document.createElement('style');
-  let minBgSize = app.logo.miniHeight !== '' ? app.logo.miniHeight + ' ' + app.logo.miniWidth : 'contain !important';
-  style.id="main-style-ui"
-  style.innerHTML = `
+  createstyles() {
+    const style = document.createElement('style');
+    let minBgSize = app.logo.miniHeight !== '' ? app.logo.miniHeight + ' ' + app.logo.miniWidth : 'contain !important';
+    style.id = "main-style-ui"
+    style.innerHTML = `
     .navbar{
       background-color: ${app.theme.headerBackground};
     }
     body{
       background-color: ${app.theme.headerBackground};
+    }
+    /* .nav-tabs {
+      background-color: ${app.theme.headerBackground};
+     } */
+    .panel-body {	
+    background-color: ${app.theme.menuBackground};
+    }
+    .panel-default > .panel-heading {
+      background-color: ${app.theme.menuBackground} !important;
+      ${app.theme.textMenuStyle}
     }
     .featureInfo h4{
       border-bottom:3px solid ${app.theme.menuBackground};
@@ -22,18 +32,15 @@ class StylesUI {
       border-bottom: 1px dashed ${app.theme.menuBackground};
     }
     .nav-tabs > li.active > a {
-      background-color: ${app.theme.activeLayer};
+      background-color: ${app.theme.activeLayer} !important;
     }
     .active {
-      background-color: ${app.theme.activeLayer};
-    }
-    .list-group-item.active, .list-group-item.active:focus, .list-group-item.active:hover{
       background-color: ${app.theme.activeLayer} !important;
     }
     .featureInfo h4 {
     border-bottom: 3px solid ${app.theme.menuBackground};
     }
-   .featuresGroup {
+    .featuresGroup {
     border-bottom: 2px solid ${app.theme.menuBackground};
     }
     .individualFeature {
@@ -41,6 +48,13 @@ class StylesUI {
     }
     .active-layers-counter {
       background: ${app.theme.activeLayer} !important;
+    }
+    .panel-default > .panel-heading {
+      color:${app.theme.textMenu};
+    }
+    .item-group-short-desc a {
+      color:${app.theme.textLegendMenu};
+      ${app.theme.textLegendMenuStyle}
     }
     .navbar-toggle .icon-bar {
       border: 1px solid ${app.theme.iconBar};
@@ -67,59 +81,58 @@ class StylesUI {
         background-position: left 1px center;
         ${app.logo.style}
       }
-
     }
     `;
 
-  let logoText = document.getElementById("logoText");
-  if (app.logoText) {
-    logoText.innerHTML= app.logoText.content;
-    logoText.href = app.logoText.link ?? '';
-    logoText.title = app.logoText.title ?? '';
-    logoText.target = '_blank';
+    let logoText = document.getElementById("logoText");
+    if (app.logoText) {
+      logoText.innerHTML = app.logoText.content;
+      logoText.href = app.logoText.link ?? '';
+      logoText.title = app.logoText.title ?? '';
+      logoText.target = '_blank';
+    }
+
+
+    document.head.appendChild(style);
+    let linkicon = document.createElement("link")
+    linkicon.rel = "icon"
+    linkicon.href = app.favicon
+    document.head.appendChild(linkicon);
+
+    if (app.title !== "") { document.title = app.title };
+
+    let topleftlogolink = document.getElementById("top-left-logo-link")
+    topleftlogolink.href = app.logo.link ?? '';
+
+    let topleftlogo = document.getElementById("top-left-logo")
+    topleftlogo.alt = app.logo.title
+    topleftlogo.title = app.logo.title
+
+    if (app.referencias.show) {
+      let toprightlogo = document.getElementById("top-right-logo")
+      toprightlogo.src = app.referencias.icon
+      toprightlogo.alt = "Referencias"
+      toprightlogo.title = "Referencias"
+      toprightlogo.style.width = app.referencias.width
+      toprightlogo.style.height = app.referencias.height
+      toprightlogo.style.top = "7px"
+
+      let image = app.referencias.src ?? "";
+
+      toprightlogo.onclick = function () {
+        clickReferencias(image)
+      };
+    }
+    /* else {
+        let toprightlogo = document.getElementById("logo-help");
+        toprightlogo.style.display = "none";
+    } */
   }
-  
 
-  document.head.appendChild(style);
-  let linkicon =  document.createElement("link")
-  linkicon.rel = "icon"
-  linkicon.href = app.favicon
-  document.head.appendChild(linkicon);
-
-  if (app.title !== "") { document.title = app.title };
-
-  let topleftlogolink = document.getElementById("top-left-logo-link")
-  topleftlogolink.href = app.logo.link ?? '';
-
-  let topleftlogo = document.getElementById("top-left-logo")
-  topleftlogo.alt = app.logo.title
-  topleftlogo.title = app.logo.title
-
-  if(app.referencias.show){
-    let toprightlogo = document.getElementById("top-right-logo")
-    toprightlogo.src = app.referencias.icon
-    toprightlogo.alt = "Referencias"
-    toprightlogo.title = "Referencias"
-    toprightlogo.style.width = app.referencias.width
-    toprightlogo.style.height = app.referencias.height
-    toprightlogo.style.top = "7px"
-
-    let image = app.referencias.src ?? "";
-
-    toprightlogo.onclick = function () {
-      clickReferencias(image)
-    };
-  } 
-  /* else {
-      let toprightlogo = document.getElementById("logo-help");
-      toprightlogo.style.display = "none";
-  } */
- }
-
- createdarktheme(){
-  const style = document.createElement('style');
-  style.id="darktheme"
-  style.innerHTML = `
+  createdarktheme() {
+    const style = document.createElement('style');
+    style.id = "darktheme"
+    style.innerHTML = `
     .navbar{
       background-color: #164A5E;
     }
@@ -131,7 +144,6 @@ class StylesUI {
     }
     .panel-default > .panel-heading {
       background-color: #164A5E !important;
-
     }
     .featureInfo h4{
       border-bottom:3px solid #164A5E;
@@ -152,7 +164,7 @@ class StylesUI {
     .featureInfo h4 {
     border-bottom: 3px solid #164A5E;
     }
-   .featuresGroup {
+    .featuresGroup {
     border-bottom: 2px solid #164A5E;
     }
     .individualFeature {
@@ -173,19 +185,17 @@ class StylesUI {
     }
     #sidebar-container{
       background-color:#164A5E;
-    }
-
+    } 
     `;
     document.head.appendChild(style);
- }
+  }
 }
 
-function clickReferencias(img){
-    event.preventDefault();
-    $.fancybox.open({
-        src : img,
-        type : 'image',
-        closeBtn: 'true'
-    });
-
+function clickReferencias(img) {
+  event.preventDefault();
+  $.fancybox.open({
+    src: img,
+    type: 'image',
+    closeBtn: 'true'
+  });
 }
