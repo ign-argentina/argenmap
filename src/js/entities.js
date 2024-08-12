f89ed29b6f18977915abd2917cbf7153365b5f2a
zalitoar <3695043+Zalitoar@users.noreply.github.com>
2024-08-12 13:31:20 -0300

    comment on possible deprecated funcion

--- src/js/entities.js
+++ src/js/entities.js
@@ -755,7 +755,7 @@ class LayersInfoWMS extends LayersInfo {
               const iBoundingBox = layer.querySelector("BoundingBox");
               const iSrs = iBoundingBox
                 ? iBoundingBox.getAttribute("srs") ||
-                  iBoundingBox.getAttribute("crs")
+                iBoundingBox.getAttribute("crs")
                 : null;
               const iMaxY = iBoundingBox
                 ? iBoundingBox.getAttribute("maxy")
@@ -777,34 +777,34 @@ class LayersInfoWMS extends LayersInfo {
               const capa =
                 thisObj.type === "wmslayer_mapserver"
                   ? new CapaMapserver(
-                      iName,
-                      iTitle,
-                      iSrs,
-                      thisObj.host,
-                      thisObj.service,
-                      thisObj.version,
-                      thisObj.feature_info_format,
-                      iMinX,
-                      iMaxX,
-                      iMinY,
-                      iMaxY,
-                    )
+                    iName,
+                    iTitle,
+                    iSrs,
+                    thisObj.host,
+                    thisObj.service,
+                    thisObj.version,
+                    thisObj.feature_info_format,
+                    iMinX,
+                    iMaxX,
+                    iMinY,
+                    iMaxY,
+                  )
                   : new Capa(
-                      iName,
-                      iTitle,
-                      iSrs,
-                      thisObj.host,
-                      thisObj.service,
-                      thisObj.version,
-                      thisObj.feature_info_format,
-                      keywords,
-                      iMinX,
-                      iMaxX,
-                      iMinY,
-                      iMaxY,
-                      null,
-                      ilegendURL,
-                    );
+                    iName,
+                    iTitle,
+                    iSrs,
+                    thisObj.host,
+                    thisObj.service,
+                    thisObj.version,
+                    thisObj.feature_info_format,
+                    keywords,
+                    iMinX,
+                    iMaxX,
+                    iMinY,
+                    iMaxY,
+                    null,
+                    ilegendURL,
+                  );
 
               // Create menu item
               const item = new Item(
@@ -917,11 +917,11 @@ class LayersInfoWMS extends LayersInfo {
     // Load geoserver Capabilities, if success Create menu and append to DOM
     $("#temp-menu").load(
       thisObj.getHostOWS() +
-        "?service=" +
-        thisObj.service +
-        "&version=" +
-        thisObj.version +
-        "&request=GetCapabilities",
+      "?service=" +
+      thisObj.service +
+      "&version=" +
+      thisObj.version +
+      "&request=GetCapabilities",
       function () {
         var capability = $("#temp-menu").find("capability");
         var keywordHtml = $("#temp-menu").find("Keyword");
@@ -1678,9 +1678,9 @@ class ItemGroup extends ItemComposite {
     if (iCapasVisibles > 0) {
       $("#" + this.getId() + "-a").html(
         this.nombre +
-          " <span class='active-layers-counter'>" +
-          iCapasVisibles +
-          "</span>",
+        " <span class='active-layers-counter'>" +
+        iCapasVisibles +
+        "</span>",
       );
     } else {
       $("#" + this.getId() + "-a").html(this.nombre);
@@ -1696,7 +1696,7 @@ class ItemGroup extends ItemComposite {
     }
   }
 
-  hideAllLayersExceptOne(item) {}
+  hideAllLayersExceptOne(item) { }
 
   getAvailableTags() {
     var availableTags = [];
@@ -2188,13 +2188,13 @@ class GestorMenu {
     return Object.keys(this.layersDataForWfs).length === 0
       ? []
       : activeLayers.map((activeLayer) => {
-          if (
-            this.layersDataForWfs.hasOwnProperty(activeLayer) &&
-            this.layersDataForWfs[activeLayer]
-          ) {
-            return this.layersDataForWfs[activeLayer];
-          }
-        });
+        if (
+          this.layersDataForWfs.hasOwnProperty(activeLayer) &&
+          this.layersDataForWfs[activeLayer]
+        ) {
+          return this.layersDataForWfs[activeLayer];
+        }
+      });
   }
 
   addActiveLayer(layer_id) {
@@ -2657,23 +2657,23 @@ class GestorMenu {
           for (var keyItem in item.itemsComposite) {
             if (
               item.itemsComposite[keyItem].capa.host ==
-                this._layersJoin[keyJoin].host &&
+              this._layersJoin[keyJoin].host &&
               item.itemsComposite[keyItem].capa.nombre ==
-                this._layersJoin[keyJoin].layer
+              this._layersJoin[keyJoin].layer
             ) {
               //Busca las capas a incluir
               for (var keyJoinInt in this._layersJoin[keyJoin].joins) {
                 var itemInt =
                   this.items[
-                    this._layersJoin[keyJoin].joins[keyJoinInt].seccion
+                  this._layersJoin[keyJoin].joins[keyJoinInt].seccion
                   ];
                 if (itemInt) {
                   for (var keyItemInt in itemInt.itemsComposite) {
                     if (
                       itemInt.itemsComposite[keyItemInt].capa.host ==
-                        this._layersJoin[keyJoin].joins[keyJoinInt].host &&
+                      this._layersJoin[keyJoin].joins[keyJoinInt].host &&
                       itemInt.itemsComposite[keyItemInt].capa.nombre ==
-                        this._layersJoin[keyJoin].joins[keyJoinInt].layer
+                      this._layersJoin[keyJoin].joins[keyJoinInt].layer
                     ) {
                       item.itemsComposite[keyItem].capas = item.itemsComposite[
                         keyItem
@@ -2766,10 +2766,10 @@ class GestorMenu {
       aSections[this._tabs[key].getExtendedId()] = [];
       aSections[this._tabs[key].getExtendedId()].push(
         "<div role='tabpanel' class='tab-pane " +
-          sClassAux +
-          "' id='" +
-          this._tabs[key].getExtendedId() +
-          "'>",
+        sClassAux +
+        "' id='" +
+        this._tabs[key].getExtendedId() +
+        "'>",
       );
       aSections[this._tabs[key].getExtendedId()].push(
         this._tabs[key].getInitialPrint(),
@@ -3305,22 +3305,22 @@ class GestorMenu {
       let lyr = layer.capa;
       lyr.nombre === layerName
         ? (layerData = {
-            name: lyr.nombre,
-            title: lyr.titulo,
-            url: lyr.host.substring(0, lyr.host.lastIndexOf("/")),
-            keywords: lyr.keywords ?? [],
-            icon: lyr.legendURL,
-            bbox: {
-              sw: {
-                lng: lyr.minx,
-                lat: lyr.miny,
-              },
-              ne: {
-                lng: lyr.maxx,
-                lat: lyr.maxy,
-              },
+          name: lyr.nombre,
+          title: lyr.titulo,
+          url: lyr.host.substring(0, lyr.host.lastIndexOf("/")),
+          keywords: lyr.keywords ?? [],
+          icon: lyr.legendURL,
+          bbox: {
+            sw: {
+              lng: lyr.minx,
+              lat: lyr.miny,
             },
-          })
+            ne: {
+              lng: lyr.maxx,
+              lat: lyr.maxy,
+            },
+          },
+        })
         : "";
     });
     return layerData ?? {};
@@ -3781,6 +3781,7 @@ class Menu_UI {
     };
 
     img_legend.addEventListener("load", (event) => {
+      /* This code seems to be replaced adaptToImage function in functions.js */
       if (img_legend.naturalHeight > 22) {
         capa_legend_div.removeChild(img_legend);
         capa_legend_div.append(resize_img_icon);
@@ -4303,14 +4304,12 @@ class Fechaimagen {
             resolution: md.SRC_RES,
             accuracy: md.SRC_ACC,
             sensor: sensorData[md.SRC_DESC]
-              ? `<a href="${sensorData[md.SRC_DESC].link}" target="_blank">${
-                  sensorData[md.SRC_DESC].name
-                }</a>`
+              ? `<a href="${sensorData[md.SRC_DESC].link}" target="_blank">${sensorData[md.SRC_DESC].name
+              }</a>`
               : md.SRC_DESC,
             provider: providerData[md.NICE_DESC]
-              ? `<a href="${providerData[md.NICE_DESC].link}" target="_blank">${
-                  providerData[md.NICE_DESC].name
-                }</a>`
+              ? `<a href="${providerData[md.NICE_DESC].link}" target="_blank">${providerData[md.NICE_DESC].name
+              }</a>`
               : md.NICE_DESC,
             sensor_texto: sensorData[md.SRC_DESC].name,
             provider_texto: providerData[md.NICE_DESC].name,
